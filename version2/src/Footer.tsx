cfb189758e4c0a819ccec617e9611abf17d0cd47
Bherna33 <brandenjhernandez@gmail.com>
2025-04-27 18:39:52 -0500

    Updating Website Im updating my website so that it would be Windows XP. Its time for something new.

---
+++ version2/src/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <footer className="border-blue-500 border-2 fixed bottom-0 right-0 left-0">
+            start
+        </footer>
+    )
+}
---
+++ version2/src/Icon.tsx
@@ -0,0 +1,12 @@
+export default function Icon(){
+    return (
+        <div className="border-2 h-24 w-24 ">
+            <div className="m-4">
+                icon
+            </div>
+            <div className="m-4">
+                label
+            </div>
+        </div>
+    )
+}
