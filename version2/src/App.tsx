689701864046194f181ffedbc079f5855ee65ebe
Bherna33 <brandenjhernandez@gmail.com>
2025-04-30 23:05:39 -0500

    added modal

--- version2/src/App.tsx
+++ version2/src/App.tsx
@@ -1,13 +1,40 @@
 import Footer from "./Footer";
 import Icon from "./Icon";
+import Modal from "./Model";
+import { useState } from 'react';
 
 export default function App() {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const openModal = () => {
+	  setIsModalOpen(true);
+	};
+  
+	const closeModal = () => {
+	  setIsModalOpen(false);
+	};
 	return (
 		<div className="h-screen w-screen bg-cover bg-center " style={{ backgroundImage: "url('/windows-xp.jpg')" }} >
 			<Icon/>
 			<Icon/>
+
+			<div className="p-4">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={openModal}>
+        Open Modal
+      </button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+          Modal Title
+        </h3>
+        <div className="mt-2">
+          <p className="text-sm text-gray-500">
+            This is the content of the modal.
+          </p>
+        </div>
+      </Modal>
+    </div>
 			
 			<Footer/>
 		</div>
-)
+	)
 }
